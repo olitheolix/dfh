6cee45bfbc573a87e8be9abaf39b8a5bef7a7abb
Oliver Nagy <olitheolix@gmail.com>
2024-11-06 17:01:03 +1100

    fix: force K8s liveness/readiness probe scheme to be HTTP.

--- backend/dfh/models.py
+++ backend/dfh/models.py
@@ -109,7 +109,7 @@ class K8sDestinationRule(BaseModel):
 class K8sProbeHttp(BaseModel):
     path: str = ""
     port: int = 0
-    scheme: str = "HTTP"
+    scheme: str = ""
 
 
 class K8sProbe(BaseModel):
